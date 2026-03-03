b3ff093217c6138dd4e6da0a6ac1588f8cc901f5
yoshiInGit <yoshimain016@gmail.com>
2026-03-03 09:05:08 +0900

    re impl userRepository

--- backend/src/main/java/com/yoshi/sleep_log/domain/repository/UserRepository.java
+++ backend/src/main/java/com/yoshi/sleep_log/domain/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package com.yoshi.sleep_log.domain.repository;
 
 import com.yoshi.sleep_log.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
-public interface UserRepository {
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 }
